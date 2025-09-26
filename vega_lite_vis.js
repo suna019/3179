2f57b15e96e8988f69fb429d20848b48a8ec71ee
suna019 <139321643+suna019@users.noreply.github.com>
2025-09-26 17:19:02 +1000

    Delete vega_lite_vis.js

--- vega_lite_vis.js
+++
@@ -1,8 +0,0 @@
-var vg_1 = "daily_cost_bar_chart.vg.json";
-
-vegaEmbed("#bar_chart", vg_1).then(function(result){
-    //Accesss the Vega view instance
-    var view = result.view;
-    console.log("Chart loaded:", view);
-}).catch(console.error);
-
