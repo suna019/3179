37670f7631524dea8a6360daaefc2f807f85b684
suna019 <139321643+suna019@users.noreply.github.com>
2025-10-21 14:40:51 +1100

    Delete HW directory

--- HW/W9 HW/vega_lite_vis.js
+++
@@ -1,7 +0,0 @@
-var vg_1 = "FIT3179_W9_HW.json";
-
-vegaEmbed("#vis", vg_1).then(function(result){
-    //Accesss the Vega view instance
-    var view = result.view;
-    console.log("Map loaded:", view);
-}).catch(console.error);
